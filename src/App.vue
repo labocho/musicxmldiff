1b814f252068d2b9affbc137496e181fdc37400d
labocho <labocho@penguinlab.jp>
2022-02-24 13:51:51 +0900

    loading files

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>Hello {{ name }}!</div>
+  <div>
+    <input type="file" @change="changeFile($event, 'A')" />
+    <input type="file" @change="changeFile($event, 'B')" />
+    <code>{{fileContentA}}</code>
+  </div>
 </template>
 
 
@@ -8,7 +12,18 @@ export default {
   data() {
     return {
       name: "Vue" as String,
+      fileContentA: "",
+      fileContentB: "",
     };
+  },
+  methods: {
+    changeFile(e, identifier) {
+      const reader = new FileReader();
+      reader.readAsText(e.currentTarget.files[0]);
+      reader.onload = (e2) => {
+        this[`fileContent${identifier}`] = e2.target.result;
+      };
+    }
   },
 };
 </script>
