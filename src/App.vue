e903362c97ceeecc7a04e5c06fe6869f68de8310
labocho <labocho@penguinlab.jp>
2022-02-24 15:11:25 +0900

    各小節の digest を取得

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,10 @@
       <ul>
         <li v-for="part in scoreA.partList" :key="part">{{part}}</li>
       </ul>
+      <ul>
+        <li v-for="digest, index in scoreA.digests" :key="index">{{digest}}</li>
+      </ul>
+
     </div>
     <div v-if="scoreB">
       <code>{{scoreB.numberOfMeasures}}</code>
@@ -22,43 +26,59 @@
 
 class Score {
   private document: Document;
-  private _numberOfMeasures: number;
-  private _partList: string[];
+  public numberOfMeasures: number;
+  public partList: string[];
+  public digests: string[];
+  private _measures: NodeListOf<Element>;
+  private update: (score: Score) => void;
 
-  constructor(xmlString: string) {
+  constructor(xmlString: string, update: (score: Score)=> void) {
+    this.update = update;
     this.document = new DOMParser().parseFromString(xmlString, "text/xml");
-    this._numberOfMeasures = this.document.querySelectorAll("measure").length;
-    this._partList = Array.prototype.map.call(
+    this.numberOfMeasures = this.document.querySelectorAll("measure").length;
+    this.partList = Array.prototype.map.call(
       this.document.querySelectorAll("part-list > score-part > part-name"),
       (el: Element) => el.textContent,
     );
+    this._measures = this.document.querySelectorAll("part#P1 > measure");
+
+    this.digests = [];
+    this.calculateDigest().then((digests) => {
+      this.digests = digests;
+      this.update(this);
+    })
   }
 
-  get numberOfMeasures(): number {
-    return this._numberOfMeasures;
+  private stringToBuffer(s: string) {
+    return new Uint16Array(Array.prototype.map.call(s, (c: string) => c.charCodeAt(0))).buffer;
   }
 
-  get partList(): string[] {
-    return this._partList;
+  private bufferToString(ab: ArrayBuffer) {
+    return String.fromCharCode.apply(null, new Uint16Array(ab));
+  }
+
+  private async calculateDigest(): Promise<string[]> {
+    const subtle = window.crypto.subtle;
+    return await Promise.all(
+      Array.prototype.map.call(
+        this._measures,
+        async (m: Element) => {
+          const ab = await subtle.digest("SHA-256", this.stringToBuffer(m.outerHTML));
+          return this.bufferToString(ab);
+        }
+      ),
+    );
   }
 }
 
 export default {
-  computed: {
-    scoreA() {
-      if (this.fileContentA === null) return null;
-      return new Score(this.fileContentA);
-    },
-    scoreB() {
-      if (this.fileContentB === null) return null;
-      return new Score(this.fileContentB);
-    },
-  },
   data() {
     return {
       name: "Vue" as String,
       fileContentA: null,
       fileContentB: null,
+      scoreA: null,
+      scoreB: null,
     };
   },
   methods: {
@@ -66,9 +86,17 @@ export default {
       const reader = new FileReader();
       reader.readAsText(e.currentTarget.files[0]);
       reader.onload = (e2) => {
-        this[`fileContent${identifier}`] = e2.target.result;
+        const xmlString = e2.target.result as string;
+        let score = null;
+        if (xmlString !== null) score = new Score(xmlString, (score: Score) => this.updateScore(score, identifier));
+
+        this[`fileContent${identifier}`] = xmlString;
+        this[`score${identifier}`] = score;
       };
-    }
+    },
+    updateScore(score: Score, identifier: string) {
+      this[`score${identifier}`] = Object.assign({}, score);
+    },
   },
 };
 </script>
