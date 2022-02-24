a6c0b2309fcccaa8be0a28add07187c7be332d3b
labocho <labocho@penguinlab.jp>
2022-02-24 12:22:04 +0900

    install Parcel

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Hello {{ name }}!</div>
+</template>
+
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      name: "Vue" as String,
+    };
+  },
+};
+</script>
