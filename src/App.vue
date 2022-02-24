fcde15a4f57332c3bf1552b353427faa8eb448b1
labocho <labocho@penguinlab.jp>
2022-02-24 14:22:56 +0900

    XML をパース

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,63 @@
   <div>
     <input type="file" @change="changeFile($event, 'A')" />
     <input type="file" @change="changeFile($event, 'B')" />
-    <code>{{fileContentA}}</code>
+    <div v-if="scoreA">
+      <code>{{scoreA.numberOfMeasures}}</code>
+      <ul>
+        <li v-for="part in scoreA.partList" :key="part">{{part}}</li>
+      </ul>
+    </div>
+    <div v-if="scoreB">
+      <code>{{scoreB.numberOfMeasures}}</code>
+      <ul>
+        <li v-for="part in scoreB.partList" :key="part">{{part}}</li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
-
 <script lang="ts">
+
+class Score {
+  private document: Document;
+  private _numberOfMeasures: number;
+  private _partList: string[];
+
+  constructor(xmlString: string) {
+    this.document = new DOMParser().parseFromString(xmlString, "text/xml");
+    this._numberOfMeasures = this.document.querySelectorAll("measure").length;
+    this._partList = Array.prototype.map.call(
+      this.document.querySelectorAll("part-list > score-part > part-name"),
+      (el: Element) => el.textContent,
+    );
+  }
+
+  get numberOfMeasures(): number {
+    return this._numberOfMeasures;
+  }
+
+  get partList(): string[] {
+    return this._partList;
+  }
+}
+
 export default {
+  computed: {
+    scoreA() {
+      if (this.fileContentA === null) return null;
+      return new Score(this.fileContentA);
+    },
+    scoreB() {
+      if (this.fileContentB === null) return null;
+      return new Score(this.fileContentB);
+    },
+  },
   data() {
     return {
       name: "Vue" as String,
-      fileContentA: "",
-      fileContentB: "",
+      fileContentA: null,
+      fileContentB: null,
     };
   },
   methods: {
