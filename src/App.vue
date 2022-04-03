fdbfc7de1fa3d6751865956762a1569067f690fd
labocho <labocho@penguinlab.jp>
2022-04-03 13:49:50 +0900

    diff で複数小節表示、リハーサルマークを除外

--- src/App.vue
+++ src/App.vue
@@ -84,22 +84,6 @@ class Measure {
     this.raw = raw;
     this.filtered = filtered;
   }
-
-  get musicXML(): string {
-    return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-      <!DOCTYPE score-partwise PUBLIC "-//Recordare//DTD MusicXML 3.1 Partwise//EN" "http://www.musicxml.org/dtds/partwise.dtd">
-      <score-partwise version="3.1">
-        <part-list>
-          <score-part id="P1">
-            <part-name>Temp</part-name>
-          </score-part>
-        </part-list>
-        <part id="P1">
-          ${this.filtered.outerHTML}
-        </part>
-      </score-partwise>
-    `;
-  }
 }
 
 class Score {
@@ -191,6 +175,7 @@ export default {
       ignores: {
         elements: [
           "sound",
+          "rehearsal",
         ],
         attributes: [
           {
--- src/Diff.vue
+++ src/Diff.vue
@@ -3,9 +3,9 @@
     <h4>A[{{diff.aMeasureNumbers.from}}..{{diff.aMeasureNumbers.to}}] is different from B[{{diff.bMeasureNumbers.from}}..{{diff.bMeasureNumbers.to}}]</h4>
     <div v-html="udiffToHTML(udiff(diff.aMeasures, diff.bMeasures))"></div>
     <div style="display: flex;">
-      <SheetMusic v-if="diff.aMeasures[0]" :musicXML="diff.aMeasures[0].musicXML" style="width: 50%" />
+      <SheetMusic v-if="diff.aMeasures[0]" :musicXML="musicXML(diff.aMeasures)" style="width: 50%" />
       <div v-else style="width: 50%" />
-      <SheetMusic v-if="diff.bMeasures[0]" :musicXML="diff.bMeasures[0].musicXML" style="width: 50%" />
+      <SheetMusic v-if="diff.bMeasures[0]" :musicXML="musicXML(diff.bMeasures)" style="width: 50%" />
       <div v-else style="width: 50%" />
     </div>
   </li>
@@ -20,6 +20,21 @@ import SheetMusic from "./SheetMusic.vue";
 export default {
   components: {SheetMusic},
   methods: {
+    musicXML(measures): string {
+      return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+        <!DOCTYPE score-partwise PUBLIC "-//Recordare//DTD MusicXML 3.1 Partwise//EN" "http://www.musicxml.org/dtds/partwise.dtd">
+        <score-partwise version="3.1">
+          <part-list>
+            <score-part id="P1">
+              <part-name>Temp</part-name>
+            </score-part>
+          </part-list>
+          <part id="P1">
+            ${measures.map((m) => m.filtered.outerHTML).join("\n")}
+          </part>
+        </score-partwise>
+      `;
+    },
     udiff(aMeasures, bMeasures) {
       let a = [];
       let b = [];
