0ffbe600fd80553b45139437bd49f2bc0e3b0dac
labocho <labocho@penguinlab.jp>
2024-03-08 14:23:44 +0900

    remove vite-plugin-electron

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,23 @@
+/** @type {import('vite').UserConfig} */
+
 import { defineConfig } from 'vite'
-import path from 'node:path'
-import electron from 'vite-plugin-electron/simple'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: "electron/main.ts",
+        preload: "electron/preload.ts",
+        renderer: "src/main.ts",
+      },
+      output: {
+        entryFileNames: "[name].js"
+      }
+    }
+  },
   plugins: [
     vue(),
-    electron({
-      main: {
-        // Shortcut of `build.lib.entry`.
-        entry: 'electron/main.ts',
-      },
-      preload: {
-        // Shortcut of `build.rollupOptions.input`.
-        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
-        input: path.join(__dirname, 'electron/preload.ts'),
-      },
-      // Ployfill the Electron and Node.js built-in modules for Renderer process.
-      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-      renderer: {},
-    }),
-  ],
+  ]
 })
