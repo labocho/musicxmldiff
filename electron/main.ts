1b0c7f1e2983dcadebcabd2c0addafde7c15e111
labocho <labocho@penguinlab.jp>
2024-03-13 16:18:11 +0900

    複数ウインドウ対応

--- electron/main.ts
+++ electron/main.ts
@@ -12,6 +12,10 @@ program
 const gotTheLock: boolean = app.requestSingleInstanceLock();
 if (!gotTheLock) app.exit();
 
+let windows: {fileA: string|null, fileB: string|null, window: any}[] = [];
+let initialOpenFiles: string[] = [];
+let appReady = false
+
 async function createWindow() {
   const win = new BrowserWindow({
     webPreferences: {
@@ -24,14 +28,52 @@ async function createWindow() {
   return win
 }
 
+function openFile(files: string[]) {
+  if (files.length === 0) {
+    return
+  } else if (files.length > 1) {
+    const fileA = files[0]
+    const fileB = files[1]
+    const w = windows.find((w) => w.fileA === null && w.fileB === null)
+    if (w === undefined) {
+      createWindow().then((newWindow) => {
+        newWindow.webContents.send("open-file", [fileA, fileB]);
+        windows.push({fileA, fileB, window: newWindow})
+        console.log(windows)
+      })
+    } else {
+      w.fileA = fileA
+      w.fileB = fileB
+      w.window.webContents.send("open-file", [fileA, fileB]);
+      if (w.window.isMinimized()) w.window.restore();
+      w.window.focus();
+      console.log(windows)
+    }
+    return
+  } else {
+    const file = files[0]
+    const w = windows.find((w) => w.fileA === null || w.fileB === null)
+    if (w === undefined) {
+      createWindow().then((newWindow) => {
+        newWindow.webContents.send("open-file", [file]);
+        windows.push({fileA: file, fileB: null, window: newWindow})
+        console.log(windows)
+      })
+    } else {
+      w.fileA === null ? (w.fileA = file) : (w.fileB = file)
+      w.window.webContents.send("open-file", [file]);
+      if (w.window.isMinimized()) w.window.restore();
+      w.window.focus();
+      console.log(windows)
+    }
+  }
+}
+
 function parseArguments(args: string[]) {
   program.parse(args, {from: "user"})
   const binary = args[0];
   return path.basename(binary) === "Electron" ? program.args.slice(2) : program.args.slice(1)
 }
-
-let currentWindow: any = null;
-let initialOpenFiles: string[] = [];
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -41,8 +83,8 @@ app.on('window-all-closed', () => {
 
 app.on("open-file", (event: Event, path: string) => {
   event.preventDefault();
-  if (currentWindow) {
-    currentWindow.webContents.send("open-file", [path]);
+  if (appReady) {
+    openFile([path])
   } else {
     initialOpenFiles.push(path);
   }
@@ -56,17 +98,12 @@ app.whenReady().then(() => {
   })
 
   app.on('second-instance', (_e: any, argv: any) => {
-    if (currentWindow.isMinimized()) currentWindow.restore();
-    currentWindow.focus();
-
-    // レンダラープロセスへファイルパスを送信
-    currentWindow.webContents.send("log", "second-instance", argv);
-    currentWindow.webContents.send('open-file', parseArguments(argv));
+    openFile(parseArguments(argv))
   });
 
-
-  ipcMain.handle("openFileDialog", async () => {
-    const { canceled, filePaths } = await dialog.showOpenDialog(currentWindow!, {properties: ['openFile']})
+  ipcMain.handle("openFileDialog", async (event: any) => {
+    const w = windows.find((e) => e.window.webContents.id === event.sender.id);
+    const { canceled, filePaths } = await dialog.showOpenDialog(w!.window, {properties: ['openFile']})
     if (!canceled) {
       return filePaths[0];
     }
@@ -81,13 +118,17 @@ app.whenReady().then(() => {
     })
   });
 
-  createWindow().then((w) => {
-    w.webContents.send("log", "launched", process.argv);
-    w.webContents.send("open-file", parseArguments(process.argv));
+  // w.webContents.send("log", "launched", process.argv);
+  openFile(parseArguments(process.argv))
+  openFile(initialOpenFiles);
 
-    for (const file of initialOpenFiles) {
-      w.webContents.send("open-file", [file]);
-    }
-    currentWindow = w
-  })
+  if (windows.length === 0) {
+    createWindow().then((newWindow) => {
+      newWindow.webContents.send("open-file", []);
+      windows.push({fileA: null, fileB: null, window: newWindow})
+      console.log(windows)
+    })
+  }
+
+  appReady = true
 })
